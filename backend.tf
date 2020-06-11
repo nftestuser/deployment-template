215bf35ba0a7c3568c8f6ac2b4e63bf49f939ab3
nftestuser <65293582+nftestuser@users.noreply.github.com>
2020-06-11 21:03:23 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,20 +1,20 @@
 
 terraform {
-  /*
   backend "s3" {
     bucket = "mm-terraform-state-test"
     key    = "terraform"
     region = "us-east-2"
   }
-  */
-
   
+
+  /*
   backend "azurerm" {
     resource_group_name   = "nfcc-functions"
     storage_account_name  = "cbtfnfccfunctions"
     container_name        = "mm-terraform-state"
     key                   = "terraform.tfstate"
   }
+  */
   
 }
 
