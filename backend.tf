44aac168b5cbd771e8789c53b5830c102c501bde
mmora <mmora@neverfail.com>
2020-06-15 20:07:43 +0300

    deploy to aws;

--- backend.tf
+++ backend.tf
@@ -1,20 +1,21 @@
 
 terraform {
-  /*
+
   backend "s3" {
     bucket = "mm-terraform-statetest"
     key    = "terraform"
     region = "us-east-2"
   }
-  */
-  
 
+  
+  /*
   backend "azurerm" {
     resource_group_name   = "nfcc-functions"
     storage_account_name  = "cbtfnfccfunctions"
     container_name        = "mm-terraform-state"
     key                   = "terraform.tfstate"
   }
+  */
 
   
 }
