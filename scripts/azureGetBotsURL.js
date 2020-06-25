bdc7830d36ffdf8e3ee13168a9737c51e8aba528
nftestuser <65293582+nftestuser@users.noreply.github.com>
2020-06-25 13:49:16 +0300

    Update azureGetBotsURL.js

--- scripts/azureGetBotsURL.js
+++ scripts/azureGetBotsURL.js
@@ -1,6 +1,11 @@
 const AzureFunctions = require('azure-functions');
 const MsRestNodeAuth = require('@azure/ms-rest-nodeauth');
 const {WebSiteManagementClient} = require('@azure/arm-appservice');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const sleepUtils = require('./sleep');
+
 
 const CLIENT_ID = process.env.ARM_CLIENT_ID;
 const CLIENT_SECRET = process.env.ARM_CLIENT_SECRET;
@@ -8,13 +13,15 @@ const CLIENT_TENANT = process.env.ARM_TENANT_ID;
 const SUBSCRIPTION_ID = process.env.ARM_SUBSCRIPTION_ID;
 
 
-async function authenticate() {
+const AZURE_FUNCTION_URL = "https://newfunctionaop.azurewebsites.net/api/testBotsUrls?code=rBsLK67mH5LFHxfEXQ3tOpG6G63d5veJBaHP2mPMPNPOSv6DdLZf6Q==";
+
+
+async function authenticate(subscriptionId) {
   let credentials = await MsRestNodeAuth.loginWithServicePrincipalSecret(CLIENT_ID, CLIENT_SECRET, CLIENT_TENANT);
   return new WebSiteManagementClient(credentials, SUBSCRIPTION_ID);
 }
 
-function parseJsonFile() {
-  const fs = require('fs');
+function parseTerraformOutputFile() {
   const rawdata = fs.readFileSync('azure_output.json');
   const elements = JSON.parse(rawdata);
   return {
@@ -23,10 +30,87 @@ function parseJsonFile() {
   }
 }
 
+function getFunctionNamesFromServerlessFile() {
+  const yaml = require('js-yaml');
+  let functionNames;
+  try {
+    const fileContents = fs.readFileSync(path.join(process.cwd(), 'serverless.yml'), 'utf8');
+    const data = yaml.safeLoad(fileContents);
+    functionNames = Object.keys(data.functions);
+    return functionNames;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function getResponse(serverlessFunctionNames, azureFunctions, masterKey) {
+  let response = {
+    data: []
+  };
+  let azureFunctionNames = [];
+  azureFunctions.forEach(azureFunction => {
+    azureFunctionNames.push(azureFunction.properties.name);
+  });
+  if (serverlessFunctionNames.length > azureFunctionNames.length) {
+    serverlessFunctionNames.forEach(serverlessFunctionName => {
+      if (azureFunctionNames.includes(serverlessFunctionName)) {
+        const azureFunction = azureFunctions.find(azureFunc => azureFunc.properties.name === serverlessFunctionName);
+        response.data.push(
+          {
+            provider: 'azure',
+            functionName: serverlessFunctionName,
+            endpointUrl: azureFunction.properties.invoke_url_template,
+            masterKey: masterKey,
+            status: 'OK'
+          }
+        );
+      } else {
+        response.data.push(
+          {
+            provider: 'azure',
+            functionName: serverlessFunctionName,
+            endpointUrl: "",
+            masterKey: masterKey,
+            status: 'failed',
+            message: 'Could not find function in azure'
+          }
+        );
+      }
+    })
+  } else {
+    azureFunctionNames.forEach(azureFunctionName => {
+      if (serverlessFunctionNames.includes(azureFunctionName)) {
+        const azureFunction = azureFunctions.find(azureFunc => azureFunc.properties.name === azureFunctionName);
+        response.data.push(
+          {
+            provider: 'azure',
+            functionName: azureFunctionName,
+            endpointUrl: azureFunction.properties.invoke_url_template,
+            masterKey: masterKey,
+            status: 'OK'
+          }
+        );
+      } else {
+        const azureFunction = azureFunctions.find(azureFunc => azureFunc.properties.name === azureFunctionName);
+        response.data.push(
+          {
+            provider: 'azure',
+            functionName: azureFunctionName,
+            endpointUrl: azureFunction.properties.invoke_url_template,
+            masterKey: masterKey,
+            status: 'failed',
+            message: 'Function is still present in azure even if was removed from package.json'
+          }
+        );
+      }
+    })
+  }
+  return response;
+}
 
 async function getEndpointsURL() {
-  const functionName = parseJsonFile().functionAppName;
-  const resourceGroupName = parseJsonFile().resourceGroupName;
+  const functionName = parseTerraformOutputFile().functionAppName;
+  const resourceGroupName = parseTerraformOutputFile().resourceGroupName;
   let response = {
     data: []
   };
@@ -42,23 +126,54 @@ async function getEndpointsURL() {
           clientSecret: CLIENT_SECRET,
           domain: CLIENT_TENANT
         });
-      let functions = await azFunctions.listFunctions();
-      let endpointUrl;
-      let functionName;
-      for (let y = 0; y < functions.length; y++) {
-        functionName = functions[y].properties.name;
-        endpointUrl = functions[y].properties.invoke_url_template;
-        response.data.push(
-        {
-          functionName: functionName,
-          endpointUrl: endpointUrl,
-          masterKey: keys.masterKey
+      let azureFunctions = await azFunctions.listFunctions();
+      let serverlessFunctionNames = getFunctionNamesFromServerlessFile();
+      let timeout = 120000;//2 min
+      while (timeout > 0) {
+        if (azureFunctions.length !== serverlessFunctionNames.length) {
+          timeout -= 5000;
+          if (timeout === 0) {
+            response = getResponse(serverlessFunctionNames, azureFunctions, keys.masterKey);
+            console.log(response);
+            return response;
+          }
+          await sleepUtils.sleep(5000);
+          azureFunctions = await azFunctions.listFunctions();
+        } else {
+          break;
         }
-      );
       }
+      let functionName;
+      for (let y = 0; y < azureFunctions.length; y++) {
+        functionName = azureFunctions[y].properties.name;
+        let endpointUrl = azureFunctions[y].properties.invoke_url_template;
+        response.data.push(
+          {
+            provider: 'azure',
+            functionName: functionName,
+            endpointUrl: endpointUrl,
+            masterKey: keys.masterKey,
+            status: 'OK'
+          }
+        );
+      }
+
     }
   }
   console.log(response);
+  return response;
+
+}
+
+async function callAzureFunction() {
+  const data = await getEndpointsURL();
+  axios({
+    method: 'post',
+    url: AZURE_FUNCTION_URL,
+    data: data
+  }).then(function (response) {
+    console.log(response.data);
+  });
 }
 
 getEndpointsURL();
