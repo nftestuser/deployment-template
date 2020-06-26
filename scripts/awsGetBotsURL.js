89438017ac52bd87df029b6aee560f969cc3c647
nftestuser <65293582+nftestuser@users.noreply.github.com>
2020-06-26 12:29:52 +0300

    Update awsGetBotsURL.js

--- scripts/awsGetBotsURL.js
+++ scripts/awsGetBotsURL.js
@@ -1,29 +1,41 @@
-function parseJsonFile() {
+const path = require('path');
+
+function parseJsonFile(filePath) {
+  const fs = require('fs');
+  const data = fs.readFileSync(filePath);
+  return JSON.parse(data);
+}
+
+
+function getEndpointsURL() {
   let response = {
-    data: []
+    mode: "Post_Bot_Deployment",
+    customer: {},
+    functions: []
   };
 
-  const fs = require('fs');
-
-  const rawdata = fs.readFileSync('output.json');
-  const elements = JSON.parse(rawdata);
-  const apiKeyValue = elements["api_key"].value.value;
-  const invokeUrl = elements["aws_api_gateway_deployment"].value.invoke_url;
-  for (let element in elements) {
+  const outputJsonData = parseJsonFile(path.join(process.cwd(), 'output.json'));
+  const packageJsonData = parseJsonFile(path.join(process.cwd(), 'package.json'));
+  const customerName = packageJsonData["customerConfig"].customerName;
+  const incrementNumber = packageJsonData["customerConfig"].incrementNumber;
+  const apiKeyValue = outputJsonData["api_key"].value.value;
+  const invokeUrl = outputJsonData["aws_api_gateway_deployment"].value.invoke_url;
+  for (let element in outputJsonData) {
     if (element !== "api_key" && element !== "aws_api_gateway_deployment") {
-      response.data.push(
+      response.functions.push(
         {
-          provider:"aws",
           functionName: element,
-          endpointUrl: invokeUrl + elements[element].value.path,
-          key: apiKeyValue
+          endpointUrl: invokeUrl + outputJsonData[element].value.path,
+          key: apiKeyValue,
+          status: "success"
         }
       );
     }
   }
-
+  response.customer.customerName = customerName;
+  response.customer.incrementNumber = incrementNumber;
   console.log(response);
   return response;
 }
 
-parseJsonFile();
+getEndpointsURL();
